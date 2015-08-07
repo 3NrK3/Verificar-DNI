51f3817cfd15f4717b22ae529a103d3f7c1d9233
Jose Collado <colladokukut@gmail.com>
2015-08-07 17:26:06 +0200

    Logica Generar Letra

---
+++ app/src/main/java/com/jcolladosp/verificardni/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.jcolladosp.verificardni;
+
+import android.support.v4.app.Fragment;
+
+public abstract class BaseFragment extends Fragment {
+
+    public BaseFragment() {
+
+    }
+}
--- app/src/main/java/com/jcolladosp/verificardni/MainActivity.java
+++ app/src/main/java/com/jcolladosp/verificardni/MainActivity.java
@@ -4,13 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener{
+
+    public TextView letrag;
+    public char letra;
+    public EditText campo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setListeners();
+        letrag = (TextView) findViewById(R.id.txLetra);
+
+        campo = (EditText) findViewById(R.id.editText);
     }
 
     @Override
@@ -35,15 +50,52 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-     public char calculaLetra(int dni)
+     public void calculaLetra(int dni)
     {
+        if(comprobarDNI(dni)){
+
         String juegoCaracteres="TRWAGMYFPDXBNJZSQVHLCKET";
         int modulo= dni % 23;
-        char letra = juegoCaracteres.charAt(modulo);
-        return letra;
+        letra = juegoCaracteres.charAt(modulo);
+        }
+
     }
     public boolean comprobarDNI(int dni){
-        String DNI =  dni;
-        if()
+
+        if(dni >0){
+            int length = (int)(Math.log10(dni)+1);
+            if(length == 8){return true;}
+            else{
+                      mostrarError();
+
+            return false;
+            }
+        }
+        return false;
+    }
+
+    public void mostrarError(){
+        Snackbar
+                .make(letrag, "Longitud del DNI incorrecta", Snackbar.LENGTH_LONG)
+                .show();
+        letrag.setText("");
+    }
+    private void setListeners() {
+        Button boton = (Button) findViewById(R.id.btGenerar);
+        boton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btGenerar) {
+            hideKeyboard();
+            if (campo.getText().toString().equals("") || campo.getText().toString().length()>=9){mostrarError();}
+            else{
+
+                int dni = Integer.parseInt(campo.getText().toString());
+                if (comprobarDNI(dni)){
+            calculaLetra(dni);
+            letrag.setText("La letra del DNI " + dni +" es " + letra);}}
+        }
     }
 }
